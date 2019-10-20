a3bc7a88c1af2155d3441bd421537ea34bc4cc8b
tcrha <thomas@9bitbyte.com>
2019-10-20 18:55:17 +1100

    added more options and print out

--- timesheet.py
+++ timesheet.py
@@ -16,7 +16,7 @@ waitMain = WebDriverWait(driver, 10)
 
 
 def login(username, password):
-
+    print("logging in")
     username_feild = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_txtUsername"]')))
     password_feild = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_txtPassword"]'))) 
     sign_in = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_cmdLogin"]')))
@@ -25,12 +25,12 @@ def login(username, password):
     password_feild.send_keys(password) 
 
     sign_in.click()
-
+    print("signed in")
     timesheet_link = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_pnlNormal"]/table/tbody/tr[4]/td/ul/li[1]/a'))) 
     timesheet_link.click()
 
 def enter_timesheets(start='09', end='18', lunch='01', submit=False):
-
+    print("entering times for one day")
     timesheet_period = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_cboAvailableTimeSheets"]'))) 
     timesheet_period.click()
     select_timesheet_period = Select(timesheet_period)
@@ -57,6 +57,7 @@ def enter_timesheets(start='09', end='18', lunch='01', submit=False):
 
     save_button = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_ASPxPanelAdd_cmdImgSave"]')))
     save_button.click()
+    print("copying over days")
 
     tues_copy_button = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_ASPxPanelMain_gvMain_cmdimggvMainCopy_1"]'))) 
     tues_copy_button.click() 
@@ -89,10 +90,20 @@ def enter_timesheets(start='09', end='18', lunch='01', submit=False):
 
     alert = driver.switch_to.alert
     alert.accept()
+    print(f"mon to friday entered, {start}00 to {end}00 with a lunch of {lunch}")
 
     if submit:
         submit = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_ASPxRoundPanel1_cmdSubmitTimesheet"]'))) 
         submit.click()
+
+        alert = driver.switch_to.alert
+        alert.accept()
+
+        submit_conformation = waitMain.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainContent_lblUpdatePanel"]')))
+        print("Submittion Successful")
+    
+    print("closing driver")
+    driver.close()
 
 if __name__ == "__main__":
     
@@ -123,7 +134,31 @@ if __name__ == "__main__":
         "-s",
         "--submit",
         action="store_true",
-        default=False
+        default=False,
+        help="submit time sheets after entering"
+    )
+
+    parser.add_argument(
+        "-a",
+        "--start",
+        type=str,
+        help="start times for days in 24h eg. 18, 09, 11: whole times are only supported",
+        default="09"
+    )
+    parser.add_argument(
+        "-e",
+        "--end",
+        type=str,
+        help="end times for days in 24h eg. 18, 09, 11: whole times are only supported",
+        default='18'
+    )
+
+    parser.add_argument(
+        "-n",
+        "--lunch",
+        type=str,
+        help="lunch period in hours eg. 01",
+        default='01'
     )
 
     args = parser.parse_args()
@@ -132,6 +167,6 @@ if __name__ == "__main__":
 
     login(args.username, args.password)
     
-    enter_timesheets(submit=args.submit)
+    enter_timesheets(submit=args.submit, start=args.start, end=args.end, lunch=args.lunch)
 
     driver.close()
